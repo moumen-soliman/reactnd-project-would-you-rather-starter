ed1e0d8e09abf2cfe538094c539b6dbccc2553c8
moumen-soliman <moumensoliman@gmail.com>
2020-04-02 15:32:18 +0200

    create utils

---
+++ src/utils/api.js
@@ -0,0 +1,14 @@
+import {
+  _getUsers,
+  _getQuestions,
+} from './_DATA.js'
+
+export function getInitialData () {
+  return Promise.all([
+    _getUsers(),
+    _getQuestions(),
+  ]).then(([users, questions]) => ({
+    users,
+    questions,
+  }))
+}
---
+++ src/utils/helpers.js
@@ -0,0 +1,27 @@
+export function formatDate (timestamp) {
+  const d = new Date(timestamp)
+  const time = d.toLocaleTimeString('en-US')
+  return time.substr(0, 5) + time.slice(-2) + ' | ' + d.toLocaleDateString()
+}
+
+function generateUID () {
+  return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
+}
+
+export function formatQuestion ({ optionOneText, optionTwoText, author }) {
+  console.log("formatQuestion");
+  console.log(optionOneText, optionTwoText, author)
+  return {
+    id: generateUID(),
+    timestamp: Date.now(),
+    author,
+    optionOne: {
+      votes: [],
+      text: optionOneText,
+    },
+    optionTwo: {
+      votes: [],
+      text: optionTwoText,
+    }
+  }
+}
